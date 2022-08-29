90ff63e3f966af4047fcffc8a110e73ae12836fd
Arman <villegasarman23@gmail.com>
2022-08-29 08:13:17 -0700

    Added new code

--- src/components/Highlights.js
+++ src/components/Highlights.js
@@ -1,32 +1,7 @@
-import {Row, Col, Button, Container, Card} from "react-bootstrap";
-import {Link} from "react-router-dom";
-import Carousel from 'react-bootstrap/Carousel';
+import React from "react";
+import Header from "../pages/Header";
 export default function Highlights(){
 	return(
-		       	<div className="container-fluid bg-image">
-			            	
-					            <Carousel>
-								      <Carousel.Item>
-								       <Col className="p-5 text-center">
-						       				<h1 className="text-success fw-bold carousel1"></h1>
-						       				
-								       </Col>
-								      </Carousel.Item>
-								      <Carousel.Item>
-								        <Col className="p-5 text-center">
-					        				<h1 className="text-success fw-bold carousel1"></h1>
-					        				
-								        </Col>
-
-								      </Carousel.Item>
-								      <Carousel.Item>
-								        <Col className="p-5 text-center">
-					        				
-					        				<h1 className="text-success fw-bold carousel1"></h1>
-								        </Col>
-								      </Carousel.Item>
-								</Carousel>
-
-			    </div>	
+		   <Header />	
 		)
-}
+};
---
+++ src/pages/Header.js
@@ -0,0 +1,53 @@
+import { Carousel } from "react-bootstrap";
+export default function Header() {
+	return(
+			<div>
+		                   <div className='container-fluid' >
+		                       <div className="row">
+		                           <div className="col-12">
+		                               <Carousel>
+		    
+		                                   <Carousel.Item>
+		                                       <img
+		                                           className="d-block w-100"
+		                                           src="https://picsum.photos/500/300?img=1"
+		                                           alt="First slide"
+		                                       />
+		                                       <Carousel.Caption>
+		                                           <h3></h3>
+		                                           <p></p>
+		                                       </Carousel.Caption>
+		                                   </Carousel.Item>
+		    
+		                                   <Carousel.Item>
+		                                       <img
+		                                           className="d-block w-100"
+		                                           src="https://picsum.photos/500/300?img=2"
+		                                           alt="Second slide"
+		                                       />
+		    
+		                                       <Carousel.Caption>
+		                                           <h3></h3>
+		                                           <p></p>
+		                                       </Carousel.Caption>
+		                                   </Carousel.Item>
+		    
+		                                   <Carousel.Item>
+		                                       <img
+		                                           className="d-block w-100"
+		                                           src="https://picsum.photos/500/300?img=3"
+		                                           alt="Third slide"
+		                                       />
+		                                       <Carousel.Caption>
+		                                           <h3></h3>
+		                                           <p></p>
+		                                       </Carousel.Caption>
+		                                   </Carousel.Item>
+		    
+		                               </Carousel>
+		                           </div>
+		                       </div>
+		                   </div>
+		               </div>	
+		)
+}
