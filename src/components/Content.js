8271799af33fa8774eb7c5160eb2d2f40e4dd277
Arman <villegasarman23@gmail.com>
2022-08-29 08:35:41 -0700

    Added new code

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,9 +1,12 @@
+import image from "react-bootstrap";
 export default function Content(){
 	return(
 		<div className=" text-center text-dark bg-light">
 			<h3>Award winning 24/7 customer support.</h3>
 			<p className="text-center">We are real people, we love our work, and we care about helping you. <br/> MBCToda is not your ordinary association. Expect extraordinary support around the clock.</p>
-	    	<div className="content"></div>
+	    	<div>
+	    		<img src="./images/img1.png" alt="image" className="img-fluid h-100"/>
+		    </div>
 	    </div>
 
 
