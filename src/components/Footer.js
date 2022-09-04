0eabaafb77c4be800f18f41fd1b5a2f76e30070e
Arman <villegasarman23@gmail.com>
2022-09-04 06:09:57 -0700

    added new code

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ export default function Footer(){
 		<div className="container-fluid p-4 text-center flex bg-dark text-light" >	
 	          	<div>
 	            	<span className="footer"><a href="">Terms and Policies</a> | <a href="">Privacy Notice</a> </span> <br/><br/>
-	            	<span> &copy;&nbsp;<strong>MBCToda</strong> 2018 - 2022.</span>
+	            	<span className="text-muted"> &copy;&nbsp;<strong>MBCToda</strong> 2018 - 2022.</span>
 	          	</div>
 		</div>
 
