cd89228f1319c8800749e33eeddb50543a27247a
Arman <villegasarman23@gmail.com>
2022-09-12 05:52:42 -0700

    new code

--- src/pages/SubNavbar.js
+++ src/pages/SubNavbar.js
@@ -1,11 +1,13 @@
 import React from "react";
+import { faPhone, faGear } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 export default function SubNavbar(){
 	return(
 		<>
 			<div id="mySidenav" className="sidenav">
 			  <a href="#" id="blog">Blog</a>
-			  <a href="#" id="projects">Projects</a>
-			  <a href="#" id="contact">Contact</a>
+			  <a href="#" id="projects"><FontAwesomeIcon icon={faGear} className="ms-1"></FontAwesomeIcon></a>
+			  <a href="#" id="contact"><FontAwesomeIcon icon={faPhone} className="ms-1"></FontAwesomeIcon></a>
 			</div>
 		</>
 		)
