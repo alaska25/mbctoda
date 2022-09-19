c9b25c25175f52a0fef17f23f4d029e308a43c96
Arman <villegasarman23@gmail.com>
2022-09-19 08:03:34 -0700

    new code

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -12,21 +12,21 @@ export default function Header() {
                            
                             <Carousel.Item>
                                 <img
-                                    className="d-block w-100"
+                                    className="d-block w-100 h-200"
                                     src="https://img.freepik.com/premium-vector/online-food-delivery-service-your-home-ordering-food-delivery-through-mobile-application-tracking-location-order-online-payment-via-phone-bank-card-time-vector-illustration_608021-1406.jpg?w=1060"
                                     alt="Third slide"
                                 />
                             </Carousel.Item>
                             <Carousel.Item>
                                 <img
-                                    className="d-block w-100"
+                                    className="d-block w-100 h-200"
                                     src="https://img.freepik.com/premium-vector/online-food-delivery-service-your-home-order-food-delivery-through-phone-app-courier-electric-scooter-map-route-hand-phone-app-eat-menu-timer-stopwatch-vector-illustration_608021-1414.jpg?w=1060"
                                     alt="Third slide"
                                 />
                             </Carousel.Item>
                             <Carousel.Item>
                                 <img
-                                    className="d-block w-100"
+                                    className="d-block w-100 h-200"
                                     src="https://img.freepik.com/free-vector/people-planning-trip-around-world-they-are-pointing-map-using-app-mobile-phone-travel-vacations-concept_1150-51089.jpg?w=1060&t=st=1663599289~exp=1663599889~hmac=d0a7dc5a7872f4e5bc299cd6f6b8f8383c2c7cd51ffafb1618be08afffec3027"
                                     alt="Third slide"
                                 />
