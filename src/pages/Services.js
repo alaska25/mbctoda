8a70d85465542030a51536b6d20a40cb8a845b13
Arman <villegasarman23@gmail.com>
2022-09-29 06:15:36 -0700

    new code

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -5,14 +5,14 @@ export default function Services(){
 	return(
 		<>
 		<div className="container">
-			<h1 className="text-center fw-bold">Our Services Available</h1>
+			<h2 className="text-center fw-bold">Our Services Available</h2>
 			<div className="d-flex align-items-center justify-content-center">
 			         <Button as={Link} to="/MemberList" eventKey="/MemberList" className="m-2 w-100 h-30 b-size">
-			         <h2>Book A Ride</h2>
+			         <h3>Book A Ride</h3>
 			         <p>MBCToda app is a ride hailing that connects commuters to available drivers.</p>
 			         </Button>
 			         <Button as={Link} to="/MemberList" eventKey="/MemberList" className="m-2 w-100 h-30">
-			         <h2>Buy/Delivery Service</h2>
+			         <h3>Buy/Delivery Service</h3>
 			         <p> Do you need to buy something? MBCToda drivers will be your personal shoppers.</p>
 			         </Button>
 			 </div>
