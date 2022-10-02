5b27c0f5eeaa3c516c76166bb12e377741e6e70f
Arman <villegasarman23@gmail.com>
2022-10-02 08:03:30 -0700

    new code

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -1,4 +1,7 @@
 import { Carousel } from "react-bootstrap";
+import img2 from "./images/pc1.png";
+import img3 from "./images/pc2.png";
+import img4 from "./images/pc3.png";
 export default function Header() {
 	return(
 		<>	
@@ -9,15 +12,22 @@ export default function Header() {
                             <Carousel.Item>
                                 <img
                                     className="d-block w-100"
-                                    src="https://img.freepik.com/premium-vector/online-food-delivery-service-your-home-fast-delivery-by-courier-tracking-your-order-through-mobile-application-map-street-city-route-time-stopwatch-menu-app-phone-vector-illustration_608021-1434.jpg?w=1060"
-                                    alt="Third slide" img-fluid
+                                    src={img4}
+                                    alt="Third slide" fluid
                                 />
                             </Carousel.Item>
                             <Carousel.Item>
                                 <img
                                     className="d-block w-100"
-                                    src="https://img.freepik.com/premium-vector/online-home-delivery-service-drone-delivering-package-food-tracking-location-order-map-future-technologies-home-courier-route-bank-card-vector-illustration_608021-1426.jpg?w=1060"
-                                    alt="Third slide" img-fluid
+                                    src={img3}
+                                    alt="Third slide" fluid
+                                />
+                            </Carousel.Item>
+                            <Carousel.Item>
+                                <img
+                                    className="d-block w-100"
+                                    src={img2}
+                                    alt="Third slide" fluid
                                 />
                             </Carousel.Item>
                        </Carousel>
